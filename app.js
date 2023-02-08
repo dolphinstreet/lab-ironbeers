da7b5aa1f7fe3fcc9ce1f864f356774f7c0fd0f2
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-08 23:52:49 +0100

    missing iteration 6 and 7

--- app.js
+++ app.js
@@ -9,17 +9,57 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(`${__dirname}/public`))
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',{
+    navbar:true,
+    title: "Home",
+    footer:true
+  });
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    res.render('beers', {
+      beers:beersFromApi,
+      navbar:true,
+      title:"Beers",
+      footer:true
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+  
+});
+
+// app.get('/beers/beer-:beerId', (req, res) => {
+//   let id = req.params;
+//   console.log("id")
+
+// }) 
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(randomBeer => {
+      res.render('random-beer',{
+        random:randomBeer,
+        navbar:true,
+        title:"Random beer",
+        footer:true
+      });
+  })
+  .catch(error => console.log(error));
+});
+
+app.listen(3000, () => console.log('🏃‍ on port http://localhost:3000'));
